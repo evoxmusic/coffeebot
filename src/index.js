488502b744fe348661d5dd7af443f6f75c0f7a01
Simeon J Morgan <smorgan@digitalfeed.net>
2020-10-05 22:08:02 +1100

    wip

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-const key = process.env.AUTH_KEY
+require('dotenv').config()
+const AUTH_KEY = process.env.AUTH_KEY
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID
+const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY
+const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME
+const AWS_BACKUP_FOLDER = process.env.AWS_BACKUP_FOLDER
+const AWS_REGION = process.env.AWS_REGION
+
 const MAX_COFFEE_ADD = 5;
 const MAX_COFFEE_SUBTRACT = 2;
 const COUNT_DISPLAY_SIZE = 5;
 
-require('dotenv').config()
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const { DateTime } = require("luxon");
 const { Pool } = require('pg');
+const AWS = require('aws-sdk');
+const CronJob = require('cron').CronJob;
 
 const app = new Koa();
 app.use(bodyParser());
@@ -22,6 +30,16 @@ const pool = new Pool({
     password: process.env.QOVERY_DATABASE_COFFEE_DB_PASSWORD,
     port: process.env.QOVERY_DATABASE_COFFEE_DB_PORT,
 });
+
+new CronJob(
+    "00 00 02 * * *",
+    async function () {
+        await createBackup()
+    },
+    null,
+    true,
+    'Australia/Melbourne'
+);
 
 function showHelp() {
     return {
@@ -41,7 +59,21 @@ function showHelp() {
 }
 
 // CREATE_DATABASE_QUERY = "CREATE DATABASE drinks ENCODING = 'UTF8'";
-CHECK_IF_DATABASE_EXISTS_QUERY = "SELECT datname FROM pg_catalog.pg_database WHERE datname = drinks;"
+// CHECK_IF_DATABASE_EXISTS_QUERY = "SELECT datname FROM pg_catalog.pg_database WHERE datname = drinks;"
+CREATE_BACKUP_TABLE_QUERY = `
+CREATE TABLE IF NOT EXISTS public.backups
+(
+    id bigserial NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    backup_until timestamp with time zone NOT NULL,
+    successful BOOLEAN NOT NULL,
+    message TEXT,
+    PRIMARY KEY (id)
+);
+`
+GET_LAST_SUCCESSFUL_BACKUP_DATETIME_QUERY = "SELECT backup_until FROM public.backups WHERE successful = TRUE ORDER BY backup_until DESC LIMIT 1"
+CREATE_BACKUP_ROW_QUERY = "INSERT INTO public.backups (created_at, backup_until, successful, message) VALUES ($1, $2, $3, $4)"
+
 CREATE_DRINK_TABLE_QUERY = `
 CREATE TABLE IF NOT EXISTS public.coffee
 (
@@ -57,11 +89,80 @@ COUNT_ALL_DRINKS_QUERY = 'SELECT COUNT(*) FROM coffee WHERE created_at > $1 AND 
 COUNT_USER_DRINKS_QUERY = 'SELECT COUNT(*) FROM coffee WHERE user_id = $1 AND created_at > $2 AND created_at < $3'
 TALLY_ALL_DRINKS_QUERY = 'SELECT user_name, COUNT(*) AS drink_count FROM coffee WHERE created_at > $1 AND created_at < $2 GROUP BY user_name ORDER BY drink_count DESC'
 DELETE_N_MOST_RECENT_DRINKS_FOR_USER_QUERY = 'DELETE FROM coffee WHERE id IN (SELECT id FROM coffee WHERE user_id = $1 AND created_at > $2 AND created_at < $3 ORDER BY id DESC LIMIT $4)'
+ALL_DRINKS_SINCE_DATETIME_QUERY = 'SELECT id, user_id, user_name, created_at FROM coffee WHERE created_at > $1'
+
+async function createBackup() {
+    const client = await pool.connect()
+
+    try {
+        const dt = DateTime.local().setZone("Australia/Melbourne");
+        const getLastSuccessfulBackupQuery = await client.query(GET_LAST_SUCCESSFUL_BACKUP_DATETIME_QUERY)
+
+        let backupFromDate = DateTime.fromSeconds(0)
+        if (getLastSuccessfulBackupQuery.rows.length > 0) {
+            backupFromDate = DateTime.fromJSDate(getLastSuccessfulBackupQuery.rows[0].backup_until)
+        }
+
+        const getAllDrinksSinceDatetimeQuery = await client.query(ALL_DRINKS_SINCE_DATETIME_QUERY, [backupFromDate.toISO()])
+        allDrinksSinceDatetime = getAllDrinksSinceDatetimeQuery.rows
+
+        if (allDrinksSinceDatetime.length === 0) {
+            return { "response_type": "ephemeral", "text": `No entries since ${backupFromDate.toISO()} to back up.` };
+        }
+
+        const rowsToBackUp = Array()
+        let maxDate = DateTime.fromSeconds(0)
+
+        for (let idx = 0, len = allDrinksSinceDatetime.length; idx < len; idx++) {
+            rowsToBackUp.push(
+                JSON.stringify({
+                    "id": allDrinksSinceDatetime[idx].id,
+                    "user_id": allDrinksSinceDatetime[idx].user_id,
+                    "user_name": allDrinksSinceDatetime[idx].user_name,
+                    "created_at": allDrinksSinceDatetime[idx].created_at,
+                })
+            )
+            let thisDate = DateTime.fromJSDate(allDrinksSinceDatetime[idx].created_at)
+            console.log(thisDate)
+            if (thisDate > maxDate) {
+                maxDate = thisDate
+            }
+        }
+
+        const s3 = new AWS.S3({
+            accessKeyId: AWS_ACCESS_KEY_ID,
+            secretAccessKey: AWS_SECRET_KEY,
+            region: AWS_REGION
+        });
+
+        const params = {
+            Bucket: AWS_BUCKET_NAME,
+            Key: `${AWS_BACKUP_FOLDER}/${maxDate.toISO()}.rows.json`,
+            Body: rowsToBackUp.join("\n")
+        };
+
+        try {
+            await s3.upload(params).promise();
+            await client.query(CREATE_BACKUP_ROW_QUERY, [dt.toISO(), maxDate.toISO(), true, ""])
+            return { "response_type": "ephemeral", "text": `${allDrinksSinceDatetime.length} entries backed up. Filename: ${params.Key}.` };
+        } catch (err) {
+            await client.query(CREATE_BACKUP_ROW_QUERY, [dt.toISO(), maxDate.toISO(), false, err])
+            return { "response_type": "ephemeral", "text": `Backup error: ${err}` };
+        }
+
+    } finally {
+        client.release()
+    }
+}
 
 async function createDatabaseBitsIfMissing() {
     const client = await pool.connect()
     try {
-        client.query(CREATE_DRINK_TABLE_QUERY);
+        console.log("Attempting to create drink table")
+        await client.query(CREATE_DRINK_TABLE_QUERY);
+        console.log("Attempting to create backup table")
+        await client.query(CREATE_BACKUP_TABLE_QUERY);
+        console.log("All table creation complete");
     } finally {
         client.release()
     }
@@ -73,7 +174,7 @@ async function showCoffeeCount(numOfItems) {
     const client = await pool.connect()
     try {
 
-        var dt = DateTime.local().setZone("Australia/Melbourne");
+        const dt = DateTime.local().setZone("Australia/Melbourne");
         const start_of_today = dt.set({ hour: 0, minute: 0, second: 0, millisecond: 0 })
         const start_of_tomorrow = dt.plus({ days: 1 }).set({ hour: 0, minute: 0, second: 0, millisecond: 0 })
 
@@ -167,13 +268,11 @@ async function addCoffee(userId, userName, inc) {
 }
 
 router.post('/addCoffee', async (ctx, next) => {
-    if (ctx.request.query.key !== key) {
+    if (ctx.request.query.key !== AUTH_KEY) {
+        console.log(ctx.request.query.key)
         ctx.body = { result: "nope" };
         return
     }
-
-    // I'd love to run this somewhere else... but where?
-    await createDatabaseBitsIfMissing();
 
     if (ctx.request.body.command !== "/coffee") {
         ctx.body({ "response_type": "ephemeral", "text": "Something has gone horribly wrong" });
@@ -195,8 +294,11 @@ router.post('/addCoffee', async (ctx, next) => {
     } else if (!isNaN(parseInt(ctx.request.body.text, 10))) {
         ctx.body = await addCoffee(ctx.request.body.user_id, ctx.request.body.user_name, parseInt(ctx.request.body.text, 10));
         return;
-    } else if (req.body.text === "") {
+    } else if (ctx.request.body.text === "") {
         ctx.body = await addCoffee(ctx.request.body.user_id, ctx.request.body.user_name, 1);
+        return;
+    } else if (ctx.request.body.text === "backup") {
+        ctx.body = await createBackup();
         return;
     } else {
         ctx.body = { "response_type": "ephemeral", "text": "I'm afraid I don't understand your command. Take another sip and try again." };
@@ -207,13 +309,17 @@ router.post('/addCoffee', async (ctx, next) => {
 app.use(router.routes())
     .use(router.allowedMethods())
 
-app.listen(3000, () => {
+app.listen(3000, async () => {
+    await createDatabaseBitsIfMissing();
+
     console.log('running on port 3000');
+
     console.log({
         user: process.env.QOVERY_DATABASE_COFFEE_DB_USERNAME,
         host: process.env.QOVERY_DATABASE_COFFEE_DB_HOST,
         database: process.env.QOVERY_DATABASE_COFFEE_DB_DATABASE,
         password: process.env.QOVERY_DATABASE_COFFEE_DB_PASSWORD,
-        port: process.env.QOVERY_DATABASE_COFFEE_DB_PORT
+        port: process.env.QOVERY_DATABASE_COFFEE_DB_PORT,
+        key: process.env.AUTH_KEY,
     });
 });
